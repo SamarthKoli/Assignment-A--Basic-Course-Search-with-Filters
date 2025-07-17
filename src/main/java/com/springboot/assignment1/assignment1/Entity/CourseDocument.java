e4bc3fd3af74c22eeffccac4a385d168c0360a29
samarthkoli <154528119+SamarthKoli@users.noreply.github.com>
2025-07-17 19:24:41 +0530

    Define CourseDocument entity and SearchRequest entity  for Elasticsearch indexing

---
+++ src/main/java/com/springboot/assignment1/assignment1/Entity/CourseDocument.java
@@ -0,0 +1,57 @@
+package com.springboot.assignment1.assignment1.Entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(indexName = "courses")
+public class CourseDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Keyword)
+    private String type;
+
+    private String gradeRange;
+
+    @Field(type = FieldType.Integer)
+    private int minAge;
+
+    @Field(type = FieldType.Integer)
+    private int maxAge;
+
+    @Field(type = FieldType.Double)
+    private double price;
+
+@Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+private LocalDateTime nextSessionDate;
+
+
+}
---
+++ src/main/java/com/springboot/assignment1/assignment1/Entity/SearchRequest.java
@@ -0,0 +1,21 @@
+package com.springboot.assignment1.assignment1.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchRequest {
+    private String q;
+    private Integer minAge, maxAge;
+    private String category, type;
+    private Double minPrice, maxPrice;
+    private String startDate;
+    private String sort; // priceAsc, priceDesc, upcoming
+    private int page = 0;
+    private int size = 10;
+    private String sortDirection = "asc";
+}
+
